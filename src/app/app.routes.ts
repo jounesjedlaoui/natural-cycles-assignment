7323f466b9046ce45a3a72e99e4026897baf452c
Jounes <jefrey776@gmail.com>
2024-09-09 11:44:42 +0200

    add home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/natural-cycles-assignment', pathMatch: 'full' }
+    { path: 'natural-cyles-assignment', component: AppComponent },
+    { path: '', redirectTo: 'natural-cycles-assignment', pathMatch: 'full' }
 ];
